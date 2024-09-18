1fe3ee0049458469f2d2b065a171bae4fda4626b
JacksonCliff <aao.myprofile@gmail.com>
2024-09-18 13:21:22 +0630

    Comfort Verses added to RAM

---
+++ assets/Data/VerseData.tsx
@@ -0,0 +1,130 @@
+const comfortVerses = [
+    {
+        id: 1,
+        verse: "Yea, though I walk through the valley of the shadow of death, I will fear no evil; for You are with me; Your rod and Your staff, they comfort me. … Surely goodness and mercy shall follow me all the days of my life; and I will dwell in the house of the LORD forever.",
+        meditation: "No matter how difficult life becomes, we are never alone. God walks with us through every trial, and His comfort can give us peace even in the midst of pain. The promise of His presence—\"for You are with me\"—means that fear loses its power, because we know that God is by our side, protecting and guiding us.His love and faithfulness are constant, and they will continue to be with us throughout our lives. This leads to the ultimate promise: \"I will dwell in the house of the LORD forever.\" This assurance of eternal life with God provides hope beyond any temporary hardship we face here on earth.",
+        reference : "Psalm 23:4, 6"
+    },
+    {
+        id : 2,
+        verse : "For His anger is but for a moment, His favor is for life; weeping may endure for a night, but joy comes in the morning.",
+        meditation : "God's anger or the difficulties He allows us to go through are temporary and serve a purpose—often to refine, teach, or correct us. However, His favor, love, and grace are lasting and constant. His favor remains over our lives, even in the midst of struggles.The imagery of \"night\" represents times of sadness, hardship, or suffering. It acknowledges that there will be times in life where we face real sorrow, and it’s okay to grieve or struggle in those moments. However, the promise is that this weeping is temporary—it lasts for a \"night,\" but joy is coming. The \"morning\" symbolizes a new beginning, hope, and the restoration of joy.hold on through the tough times, knowing that God’s joy and deliverance will come." ,
+        reference : "Psalm 30:5",
+    },
+    {
+        id : 3,
+        verse : "He heals the brokenhearted and binds up their wounds.",
+        meditation : "",
+        reference : "Psalm 147:3",
+    },
+    {
+        id : 4,
+        verse : "‘Comfort, yes, comfort My people!’ says your God.",
+        meditation : "",
+        reference : "Isaiah 40:1",
+    },
+    {
+        id : 5,
+        verse : "Blessed are those who mourn, for they shall be comforted.",
+        meditation : "",
+        reference : "Matthew 5:4",
+    },
+    {
+        id : 6,
+        verse : "Come to Me, all you who labor and are heavy laden, and I will give you rest.",
+        meditation : "",
+        reference : "Matthew 11:28",
+    },
+    {
+        id : 7,
+        verse : "For whatever things were written before were written for our learning, that we through the patience and comfort of the Scriptures might have hope.",
+        meditation : "",
+        reference : "Romans 15:4",
+    },
+    {
+        id : 8,
+        verse : "Blessed be the God and Father of our Lord Jesus Christ, the Father of mercies and God of all comfort, who comforts us in all our tribulation, that we may be able to comfort those who are in any trouble, with the comfort with which we ourselves are comforted by God.",
+        meditation : "",
+        reference : "2 Corinthians 1:3-4",
+    },
+    {
+        id : 9,
+        verse : "You shall increase my greatness, and comfort me on every side.",
+        meditation : "",
+        reference : "Psalm 71:21",
+    },
+    {
+        id : 10,
+        verse : "This is my comfort in my affliction, for Your word has given me life.",
+        meditation : "",
+        reference : "Psalm 119:50",
+    },
+    {
+        id : 11,
+        verse : "And in that day you will say: ‘O LORD, I will praise You; though You were angry with me, Your anger is turned away, and You comfort me.’",
+        meditation : "",
+        reference : "Isaiah 12:1",
+    },
+    {
+        id : 12,
+        verse : "Sing, O heavens! Be joyful, O earth! And break out in singing, O mountains! For the LORD has comforted His people, and will have mercy on His afflicted.",
+        meditation : "",
+        reference : "Isaiah 49:13",
+    },
+    {
+        id : 13,
+        verse : "For the LORD will comfort Zion, He will comfort all her waste places; He will make her wilderness like Eden, and her desert like the garden of the LORD; joy and gladness will be found in it, thanksgiving and the voice of melody.",
+        meditation : "",
+        reference : "Isaiah 51:3",
+    },
+    {
+        id : 14,
+        verse : "I, even I, am He who comforts you. Who are you that you should be afraid of a man who will die, and of the son of a man who will be made like grass?",
+        meditation : "",
+        reference : "Isaiah 51:12",
+    },
+    {
+        id : 15,
+        verse : "To proclaim the acceptable year of the LORD, and the day of vengeance of our God; to comfort all who mourn, to console those who mourn in Zion, to give them beauty for ashes, the oil of joy for mourning, the garment of praise for the spirit of heaviness; that they may be called trees of righteousness, the planting of the LORD, that He may be glorified.",
+        meditation : "",
+        reference : "Isaiah 61:2-3",
+    },
+    {
+        id : 16,
+        verse : "As one whom his mother comforts, so I will comfort you; and you shall be comforted in Jerusalem.",
+        meditation : "",
+        reference : "Isaiah 66:13",
+    },
+    {
+        id : 17,
+        verse : "Then shall the virgin rejoice in the dance, and the young men and the old, together; for I will turn their mourning to joy, will comfort them, and make them rejoice rather than sorrow.",
+        meditation : "",
+        reference : "Jeremiah 31:13",
+    },
+    {
+        id : 18,
+        verse : "Then the churches throughout all Judea, Galilee, and Samaria had peace and were edified. And walking in the fear of the Lord and in the comfort of the Holy Spirit, they were multiplied.",
+        meditation : "",
+        reference : "Acts 9:31",
+    },
+    {
+        id : 19,
+        verse : "Nevertheless God, who comforts the downcast, comforted us by the coming of Titus.",
+        meditation : "",
+        reference : "2 Corinthians 7:6",
+    },
+    {
+        id : 20,
+        verse : "Therefore comfort each other and edify one another, just as you also are doing.",
+        meditation : "",
+        reference : "1 Thessalonians 5:11",
+    },
+    {
+        id : 21,
+        verse : "Now may our Lord Jesus Christ Himself, and our God and Father, who has loved us and given us everlasting consolation and good hope by grace, comfort your hearts and establish you in every good word and work.",
+        meditation : "",
+        reference : "2 Thessalonians 2:16-17",
+    }
+
+]
+
